17c240420be2f28922857f2387a4caac75f00adc
Megu <jovi10161@gmail.com>
2017-11-09 01:46:27 +0000

    ree

---
+++ site/index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Testing</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- other -->
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="page-header">
+					<h1>Steam Reviews</h1>
+					<input type="text" name="search" placeholder="Search..">
+				</div>
+			</div>
+		</div>
+		<div class="jumbotron">	
+			<h2></h2>
+			<p></p>
+		</div>
+		<p>Top Rated</p>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+			<div class="col-sm-3">
+				<div class="row">
+					<div class="col-sm-10">
+						<div class="title">
+							<h6>example title</h6>
+						</div>
+					</div>
+					<div class="col-sm-2">
+						<div class="rating">
+							<h6>5/7</h6>
+						</div>
+					</div>
+				</div>
+				<a href="#"><img class="img-responsive" src="img/missing.png" alt="picture"></a>
+			</div>
+		</div>
+		<div class="page-header">
+		</div>
+	</div>
+</body>
+</html>
