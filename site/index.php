c815d7d2e06bed8478f90a6bbfce904c170813f0
Megu <jovi10161@gmail.com>
2017-11-11 15:53:06 +0000

    added carousel

--- site/index.php
+++ site/index.php
@@ -23,9 +23,57 @@
 				</div>
 			</div>
 		</div>
-		<div class="jumbotron">	
-			<h2></h2>
-			<p></p>
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+				<li data-target="#myCarousel" data-slide-to="3"></li>
+				<li data-target="#myCarousel" data-slide-to="4"></li>
+			</ol>
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<a href="#"><img src="img/ex1.jpg" alt=featured></a>
+					<div class="carousel-caption">
+						<h3>Title</h3>
+					</div>
+				</div>
+				<div class="item">
+					<a href="#"><img src="img/ex2.jpg" alt=featured></a>
+					<div class="carousel-caption">
+						<h3>Title</h3>
+					</div>
+				</div>
+				<div class="item">
+					<a href="#"><img src="img/ex3.jpg" alt=featured></a>
+					<div class="carousel-caption">
+						<h3>Title</h3>
+					</div>
+				</div>
+				<div class="item">
+					<a href="#"><img src="img/ex4.jpg" alt=featured></a>
+					<div class="carousel-caption">
+						<h3>Title</h3>
+					</div>
+				</div>
+				<div class="item">
+					<a href="#"><img src="img/ex5.jpg" alt=featured></a>
+					<div class="carousel-caption">
+						<h3>Title</h3>
+					</div>
+				</div>
+			</div>
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous"</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 		<p>Top Rated</p>
 		<div class="row">
